496b69a6de2a6cd7f447cc34c4111094f187205d
tuananh190206 <tuananh190206@gmail.com>
2025-11-15 12:07:42 +0700

    ...

--- admin/index.php
+++ admin/index.php
@@ -5,29 +5,34 @@ require_once '../commons/env.php'; // Khai báo biến môi trường
 require_once '../commons/function.php'; // Hàm hỗ trợ
 
 // Require toàn bộ file Controllers
-
 require_once './controllers/AdminDanhMucController.php';
 // require_once './controllers/AdminSanPhamController.php';
 // require_once './controllers/AdminDonHangController.php';
 // require_once './controllers/AdminBaoCaoThongKeController.php';
-// require_once './controllers/AdminTaiKhoanController.php';
-
+require_once './controllers/AdminTaiKhoanController.php';
 require_once './controllers/AdminBaoCaoThongKeController.php';
 
 // Require toàn bộ file Models
-
-// require_once './models/AdminDanhMuc.php';
+require_once './models/AdminDanhMuc.php';
 // require_once './models/AdminSanPham.php';
 // require_once './models/AdminDonHang.php';
 require_once './models/AdminTaiKhoan.php';
-require_once './models/AdminHDV.php ';
+require_once './models/AdminHDV.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
+
+
+// if ($act !== 'login-admin' && $act !== 'check-login-admin' && $act !== 'check-logout-admin') {
+//     // Kiểm tra đăng nhập admin
+//     checkLoginAdmin();
+// }
+
 // if($act!== 'login-admin'&& $act!=='check-login-admin' && $act!=='check-logout-admin'){
 //     // Kiểm tra đăng nhập admin
 //     checkLoginAdmin();
 // }
+
 
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
@@ -68,33 +73,29 @@ match ($act) {
     '/' => (new AdminBaoCaoThongKeController())->home(),
 
 
-// route quản lí tài khoản
+    // route quản lí tài khoản
 // Quản lí tài khoản quản trị
-// 'list-tai-khoan-quan-tri' => (new AdminTaiKhoanController())->danhSachQuanTri(),
-// 'form-them-quan-tri' => (new AdminTaiKhoanController())->formAddQuanTri(),
+    'list-tai-khoan-quan-tri' => (new AdminTaiKhoanController())->danhSachQuanTri(),
+    // 'form-them-quan-tri' => (new AdminTaiKhoanController())->formAddQuanTri(),
 // 'them-quan-tri' => (new AdminTaiKhoanController())->postAddQuanTri(),
 // 'form-sua-quan-tri' => (new AdminTaiKhoanController())->formEditQuanTri(),
 // 'sua-quan-tri' => (new AdminTaiKhoanController())->postEditQuanTri(),
 
-//route reset password
+    //route reset password
 // 'reset-password' => (new AdminTaiKhoanController())->resetPassword(),
 
-// // //route quản lý tài khoản khach hàng
-// 'list-tai-khoan-khach-hang' => (new AdminTaiKhoanController())->danhSachKhachHang(),
-// 'form-sua-khach-hang' => (new AdminTaiKhoanController())->formEditKhachHang(),
-// 'sua-khach-hang' => (new AdminTaiKhoanController())->postEditKhachHang(),
-// 'chi-tiet-khach-hang' => (new AdminTaiKhoanController())->detailKhachHang(),
+    // // //route quản lý tài khoản khach hàng
+    'list-tai-khoan-hdv' => (new AdminTaiKhoanController())->danhSachHDV(),
+// 'form-sua-hdv' => (new AdminTaiKhoanController())->formEditHDV(),
+// 'sua-hdv' => (new AdminTaiKhoanController())->postEditHDV(),
+// 'chi-tiet-hdv' => (new AdminTaiKhoanController())->detailHDV(),
 
 // route quản lý tài khoản cá nhân
 // 'form-sua-thong-tin-ca-nhan-quan-tri' => (new AdminTaiKhoanController())->formEditCaNhanQuanTri(),
 // // 'sua-thong-tin-ca-nhan-quan-tri'=>(new AdminTaiKhoanController())->postEditCaNhanQuanTri(),
 // 'sua-mat-khau-ca-nhan-quan-tri' => (new AdminTaiKhoanController())->postEditMatKhauCaNhan(),
 
-//route quản lý tài khoản khach hàng
-'list-tai-khoan-hdv' => (new AdminTaiKhoanController())->danhSachHDV(),
- 'form-sua-hdv' => (new AdminTaiKhoanController())->formEditHDV(),
- 'sua-hdv' => (new AdminTaiKhoanController())->postEditHDV(),
-        'chi-tiet-hdv' => (new AdminTaiKhoanController())->detailHDV(),
+
 
 //route login 
 // 'login-admin' => (new AdminTaiKhoanController())->formLogin(),
--- commons/env.php
+++ commons/env.php
@@ -1,9 +1,4 @@
 <?php
-define('BASE_URL', 'http://localhost/DU-AN-1-FANTASTIC-JOURNEYS/');
-
-//đường dẫn vào đường admin
-define('BASE_URL_ADMIN', 'http://localhost/DU-AN-1-FANTASTIC-JOURNEYS/Admin/');
-
 
 
 define('BASE_URL'       , 'http://localhost/DU-AN-1-FANTASTIC-JOURNEYS/');
